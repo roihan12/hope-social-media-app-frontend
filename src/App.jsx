41033737544c680042eb93292c269bce864032ef
Your Name <roihansori12@gmail.com>
2024-01-02 01:37:06 +0700

    feat: add login and register  page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+function App() {
+  return (
+      <div>
+        {/* <Login /> */}
+        <Register /> 
+      </div>
+  );
+}
+
+export default App;
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,34 @@
+import "./register.scss";
+
+const Register = () => {
+
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>HOPE! Social Media.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
+            veniam atque cumque ipsam quibusdam dolorem vero velit aliquid
+            aspernatur debitis!
+          </p>
+          <span>Do you have an account?</span>
+        <button>Login</button>
+        </div>
+       
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
